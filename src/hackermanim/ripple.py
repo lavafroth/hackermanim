8c2c5c0432bdde3233958dd88663bf5c302e3e65
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-07-01 17:13:00 +0530

    feat: use explicit None check for center correction

--- src/hackermanim/ripple.py
+++ src/hackermanim/ripple.py
@@ -2,10 +2,12 @@ from manim import Mobject, AnimationGroup, ParametricFunction, np, PI, UP, MoveA
 from typing import Iterable
 
 def ripple(mobjects: Iterable[Mobject], scale=0.1, t_range=[0, 1], center_correction=None) -> AnimationGroup:
+    if center_correction is None:
+        center_correction = 0.05 * UP
+
     curve = ParametricFunction(lambda t: np.array([0, scale * np.sin(PI * t), 0]), t_range=t_range)
     anims = []
     for char in mobjects:
-        center_correction = center_correction or 0.05 * UP
         curve = curve.copy().move_to(char.get_center() + center_correction)
         anims.append(MoveAlongPath(char, curve))
     return AnimationGroup(*anims, lag_ratio=0.1)
