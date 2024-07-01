f63f6cb7c22e43b8cbcbd2a97556fd9839a1e5f4
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-07-01 17:11:28 +0530

    feat: positional args for scale and time range for ripple

--- src/hackermanim/ripple.py
+++ src/hackermanim/ripple.py
@@ -1,8 +1,8 @@
 from manim import Mobject, AnimationGroup, ParametricFunction, np, PI, UP, MoveAlongPath
 from typing import Iterable
 
-def ripple(mobjects: Iterable[Mobject], center_correction=None) -> AnimationGroup:
-    curve = ParametricFunction(lambda t: np.array([0, 0.1 * np.sin(PI * t), 0]), t_range=[0, 1])
+def ripple(mobjects: Iterable[Mobject], scale=0.1, t_range=[0, 1], center_correction=None) -> AnimationGroup:
+    curve = ParametricFunction(lambda t: np.array([0, scale * np.sin(PI * t), 0]), t_range=t_range)
     anims = []
     for char in mobjects:
         center_correction = center_correction or 0.05 * UP
